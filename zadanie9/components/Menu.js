af3ee142df680c9e11a2ad3ea76271e7537527d2
bestemic <przemek.pawlik123@gmail.com>
2023-06-04 18:18:48 +0200

    Added categories in menu

--- zadanie9/components/Menu.js
+++ zadanie9/components/Menu.js
@@ -2,24 +2,50 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from "../components/Home";
 import Cart from "./Cart";
+import Products from "./Products";
+import {useEffect, useState} from "react";
+import {getCategories} from "../services/categories";
 
 const Drawer = createDrawerNavigator();
 
-const Menu = () => (
-    <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen
-                name="Home"
-                component={Home}
-                options={{drawerLabel: 'Home'}}
-            />
-            <Drawer.Screen
-                name="Cart"
-                component={Cart}
-                options={{drawerLabel: 'Cart'}}
-            />
-        </Drawer.Navigator>
-    </NavigationContainer>
-);
+const Menu = () => {
+    const [categories, setCategories] = useState([]);
+
+
+    const renderProductsScreen = (category) => {
+        console.log(category)
+        return () => <Products category={category}/>;
+    };
+
+    useEffect(() => {
+        const categories = getCategories();
+        setCategories(categories);
+    }, []);
+
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen
+                    name="Home"
+                    component={Home}
+                    options={{drawerLabel: 'Home'}}
+                />
+                <Drawer.Screen
+                    name="Cart"
+                    component={Cart}
+                    options={{drawerLabel: 'Cart'}}
+                />
+                {categories.map((category) => (
+                    <Drawer.Screen
+                        key={category.id}
+                        name={category.name}
+                        component={renderProductsScreen(category)}
+                        options={{drawerLabel: category.name}}
+                    />
+                ))}
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+}
 
 export default Menu
---
+++ zadanie9/services/categories.js
@@ -0,0 +1,16 @@
+const categories = [
+    {id: 1, name: 'Electronics'},
+    {id: 2, name: 'Fashion'},
+    {id: 3, name: 'Home Decor'},
+    {id: 4, name: 'Beauty'},
+    {id: 5, name: 'Sports'},
+    {id: 6, name: 'Books'},
+]
+
+const getCategories = () => {
+    return categories;
+};
+
+export {
+    getCategories
+}
